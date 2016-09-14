5b4e3d7d15c17da3e733cef8b16fa6a27a8beeaa
Steve Loveless <steve.loveless@gmail.com>
2016-09-13 23:19:00 -0700

    Eval input as ruby

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
 extern crate readline;
-extern crate ruosh;
 extern crate ruru;
+extern crate ruosh;
 
 use readline::*;
 use ruosh::*;
+use ruru::{Class, AnyObject, RString};
+use ruru::traits::Object;
+
+fn reval(code: String) -> AnyObject {
+    let code = RString::new(code.as_str()).to_any_object();
+
+    Class::from_existing("Kernel").send("eval", vec![code])
+}
+
+fn rputs(ruby_result: AnyObject) {
+    let inspected = ruby_result.send("inspect", vec![]);
+    Class::from_existing("Kernel").send("puts", vec![inspected]);
+}
 
 fn main() {
     internal_init();
@@ -23,11 +36,7 @@ fn main() {
         }
 
         readline::add_history(input.as_ref());
-
-        // println!("Input: '{}'", input);
-        // let ruby_command = format!("x = {}; x.to_s", input);
-        // println!("rcmd: '{}'", ruby_command);
-        // let ruby_result = mruby.run(&ruby_command).unwrap();
-        // println!("Ruby: {}", ruby_result.to_str().unwrap());
+        let ruby_result = reval(input);
+        rputs(ruby_result);
     }
 }
