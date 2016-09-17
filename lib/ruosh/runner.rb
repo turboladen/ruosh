3eb8e50fe094329ea39702c0be2e32d6080301ae
Steve Loveless <steve.loveless@gmail.com>
2016-09-17 16:36:02 -0700

    Add pipe (|>) handling
    Looks like an Elixir pipe, but yields the result of the left-hand side
    as the _receiver_ of the call on the right-hand side:
    
    ```
    >> [1, 2, 3] |> map { |x| x * 3 }
    #<Ruosh::Result:0x007fa75b091168 @object=[3, 6, 9], @command_number=2>
    ```

--- lib/ruosh/runner.rb
+++ lib/ruosh/runner.rb
@@ -1,17 +1,94 @@
 module Ruosh
   class Runner
-    attr_reader :binding
+    attr_reader :last_error, :command_history, :result_history, :input_history
 
     def initialize
       @binding = Kernel.binding
+      @last_error = nil
+      @command_number = 0
+      @command_history = []
+      @result_history = []
+      @input_history = []
+
+      # Set binding-specific $last_result
+      eval('$last_result = nil', @binding)
     end
 
+    # @param code [String]
+    # @return [Object]
     def run(code)
-      puts "Running code:\n#{code}"
-      Kernel.eval(code, @binding)
+      @input_history << code
+      code_chunks = split_from_pipes(code)
+      final_result = nil
+
+      code_chunks.each_with_index do |code_chunk, i|
+        @command_number += 1
+
+        code_chunk = %[$last_result.instance_eval("#{code_chunk}")] unless i.zero?
+
+        final_result = run_code_chunk(code_chunk, @command_number)
+      end
+
+      final_result
+    end
+
+    def last_result
+      @result_history[-2]
+    end
+
+    def last_command
+      @command_history[-2]
+    end
+
+    private
+
+    # @param code [String]
+    # @return [Array]
+    def split_from_pipes(code)
+      code.split('|>')
+    end
+
+    # @param code [String]
+    # @param cmd_number [Fixnum]
+    # @return [Ruosh::Result]
+    def run_code_chunk(code, cmd_number)
+      command = Command.new(code, cmd_number)
+      @command_history.push(command)
+      result_object = command.run(@binding)
+
+      result = Result.new(result_object, cmd_number)
+      @result_history.push(result)
+      @last_error = result if result.error?
+
+      result
+    end
+  end
+
+  class Command
+    attr_reader :code, :command_number
+
+    def initialize(code, command_number)
+      @code = code
+      @command_number = command_number
+    end
+
+    def run(binding)
+      Kernel.eval("$last_result = #{@code}", binding)
     rescue => ex
-      puts "ex: #{ex.class}, #{ex.message}"
       ex
+    end
+  end
+
+  class Result
+    attr_reader :object, :command_number
+
+    def initialize(object, command_number)
+      @object = object
+      @command_number = command_number
+    end
+
+    def error?
+      object.is_a?(Exception)
     end
   end
 end
