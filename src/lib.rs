0424892e70ce9756383a691d90ccb32a9bf30b14
Steve Loveless <steve.loveless@gmail.com>
2016-10-09 22:48:57 -0700

    No more ssh stuff

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,12 @@
-pub mod ssh;
 pub mod ruby;
 
 #[no_mangle]
 pub extern fn init_my_things() {
     ruby::init_ruby();
-    ssh::init();
 }
 
 pub fn internal_init() {
     ruby::init_ruby();
-    ssh::init();
 }
 
 #[cfg(test)]
